6fcfc2fcc92497dc6521c49b4cf310b83fbd3d9c
Pierantonio Fanigliulo <fanigliulo.pierantonio@gmail.com>
2024-10-17 19:56:20 +0200

    CodeSendingRequest phoneNumber JsonProperty

--- src/main/java/it/pierfani/smsapi/dto/CodeSendingRequest.java
+++ src/main/java/it/pierfani/smsapi/dto/CodeSendingRequest.java
@@ -1,9 +1,12 @@
 package it.pierfani.smsapi.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
 @Data
 public class CodeSendingRequest {
+    @JsonProperty("phone_number")
     private String phoneNumber;
     private String from;
     private String content;
